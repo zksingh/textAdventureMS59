e974a1d5b9214960e1753ad63750d69d585702db
Zayn Singh <ZSingh3358@scrippscollege.edu>
2019-11-15 09:18:16 -0800

    Updated player.py and game.py

--- game.py
+++ game.py
@@ -4,25 +4,57 @@ import world #to be implemented
 def play(): #create game loop 
     print("Escape from Cave Terror!") #change to my title
     player = Player() #create new player instance
-    while True: # while true
-        room = world.tile_at(player.x, player.y) #create a variable called room and set it to the current location of the player
-        print(room.introText()) #print the appropriate tile's intro text
-        actionInput = getPlayerCommand() 
+    room = world.tile_at(player.x, player.y) #create a variable called room and set it to the current location of the player
+    while player.is_alive() and not player.victory:
+        room = world.tile_at(player.x, player.y)
+        print(room.intro_text())
+        room.modify_player(player)
+        if player.is_alive() and not player.victory:
+            choose_action(room, player)
+        elif not player.is_alive():
+            print("Your journey has come to an early end!")
 
-        #actions depending on what the user types:
-        if actionInput in ['n', 'N']: 
-            player.move_north()
-        elif actionInput in ['s', 'S']:
-            player.move_south()
-        elif actionInput in ['w', 'W']:
-            player.move_west()
-        elif actionInput in ['e', 'E']:
-            player.move_east
+
+def choose_action(room, player):
+    action = None
+    while not action:
+        available_actions = get_available_actions(room, player)
+        action_input = input("Action: ")
+        action = available_actions.get(action_input)
+        if action:
+            action()
         else:
-            print("Invalid Action try again!")
+            print("Invalid action!")
 
-def getPlayerCommand():
-    return input('Action: ')
+
+def get_available_actions(room, player):
+    actions = OrderedDict()
+    print("Choose an action: ")
+    if player.inventory:
+        action_adder(actions, 'i', player.print_inventory, "Print inventory")
+    if isinstance(room, world.TraderTile):
+        action_adder(actions, 't', player.trade, "Trade")
+    if isinstance(room, world.EnemyTile) and room.enemy.is_alive():
+        action_adder(actions, 'a', player.attack, "Attack")
+    else:
+        if world.tile_at(room.x, room.y - 1):
+            action_adder(actions, 'n', player.move_north, "Go north")
+        if world.tile_at(room.x, room.y + 1):
+            action_adder(actions, 's', player.move_south, "Go south")
+        if world.tile_at(room.x + 1, room.y):
+            action_adder(actions, 'e', player.move_east, "Go east")
+        if world.tile_at(room.x - 1, room.y):
+            action_adder(actions, 'w', player.move_west, "Go west")
+    if player.hp < 100:
+        action_adder(actions, 'h', player.heal, "Heal")
+
+    return actions
+
+
+def action_adder(action_dict, hotkey, action, name):
+    action_dict[hotkey.lower()] = action
+    action_dict[hotkey.upper()] = action
+    print("{}: {}".format(hotkey, name))
+
 
 play()
-
--- player.py
+++ player.py
@@ -4,29 +4,58 @@ import items
 
 class Player:
     def __init__(self):
-        self.inventory = [items.Knife(), 'Gold(5)', 'Bread']
-        self.x = 1 
-        self.y = 2
-
-    def printInventory(self):
-        print("Inventory")
-        for item in inventory:
-            print('*' + str(item))
-        bestWeapon = self.mostPowerfulWeapon()
-        print("Your best weapon is {}.".format(bestWeapon))
+        self.inventory = [items.Knife(), items.Bread()]
+        self.x = world.start_tile_location[0]
+        self.y = world.start_tile_location[1]
+        self.hp = 100
+        self.gold = 5
+        self.victory = False
     
-    def mostPowerfulWeapon(self):
-        maxDamage = 0
-        bestWeapon = None
+    def is_alive(self):
+        return self.hp > 0
+
+    def print_inventory(self):
+        print("Inventory:")
+        for item in self.inventory:
+            print('* ' + str(item))
+        print("Gold: {}".format(self.gold))
+
+    def heal(self):
+        consumables = [item for item in self.inventory
+                       if isinstance(item, items.Consumable)]
+        if not consumables:
+            print("You don't have any items to heal you!")
+            return
+
+        for i, item in enumerate(consumables, 1):
+            print("Choose an item to use to heal: ")
+            print("{}. {}".format(i, item))
+
+        valid = False
+        while not valid:
+            choice = input("")
+            try:
+                to_eat = consumables[int(choice) - 1]
+                self.hp = min(100, self.hp + to_eat.healing_value)
+                self.inventory.remove(to_eat)
+                print("Current HP: {}".format(self.hp))
+                valid = True
+            except (ValueError, IndexError):
+                print("Invalid choice, try again.")
+
+    def most_powerful_weapon(self):
+        max_damage = 0
+        best_weapon = None
         for item in self.inventory:
             try:
-                if item.damage > maxDamage:
-                    bestWeapon = item
-                    maxDamage = item.damage 
+                if item.damage > max_damage:
+                    best_weapon = item
+                    max_damage = item.damage
             except AttributeError:
                 pass
-        return bestWeapon
-    
+
+        return best_weapon
+
     def move(self, dx, dy):
         self.x += dx
         self.y += dy
@@ -42,3 +71,14 @@ class Player:
 
     def move_west(self):
         self.move(dx=-1, dy=0)
+
+    def attack(self):
+        best_weapon = self.most_powerful_weapon()
+        room = world.tile_at(self.x, self.y)
+        enemy = room.enemy
+        print("You use {} against {}!".format(best_weapon.name, enemy.name))
+        enemy.hp -= best_weapon.damage
+        if not enemy.is_alive():
+            print("You killed {}!".format(enemy.name))
+        else:
+            print("{} HP is {}.".format(enemy.name, enemy.hp))
