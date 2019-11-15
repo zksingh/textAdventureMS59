c81fc805b2310ab454cb10f77610c3262090e65a
Zayn Singh <ZSingh3358@scrippscollege.edu>
2019-11-15 09:01:23 -0800

    finished game.py but having issues with game loop

--- game.py
+++ game.py
@@ -1,8 +1,28 @@
 from player import Player #to be implemented 
-import World #to be implemented 
+import world #to be implemented 
 
 def play(): #create game loop 
     print("Escape from Cave Terror!") #change to my title
-    player = Player()
-    while True:
-        room = world.title_at
+    player = Player() #create new player instance
+    while True: # while true
+        room = world.tile_at(player.x, player.y) #create a variable called room and set it to the current location of the player
+        print(room.introText()) #print the appropriate tile's intro text
+        actionInput = getPlayerCommand() 
+
+        #actions depending on what the user types:
+        if actionInput in ['n', 'N']: 
+            player.move_north()
+        elif actionInput in ['s', 'S']:
+            player.move_south()
+        elif actionInput in ['w', 'W']:
+            player.move_west()
+        elif actionInput in ['e', 'E']:
+            player.move_east
+        else:
+            print("Invalid Action try again!")
+
+def getPlayerCommand():
+    return input('Action: ')
+
+play()
+
--- world.py
+++ world.py
@@ -20,48 +20,53 @@ class StartTile(MapTile): #create the start tile of the world
         and rely only on yourself. Now to begin, there are four areas of the Field:
         The Northern Corridors, The Southern Corridors, The East Wall, and The West Wall. 
         """
+class BoringTile(MapTile):
+    def introText(self):
+        return """
+        This is a very boring part of the cave.
+        """
 
 class EnemyTile(MapTile):
     def __init__(self, x, y):
         r = random.random #to randomly place enemies along the tiles
         if r < 0.50:
             self.enemies = enemies.GiantRobot() #place a giant robot every .50 
-            self.aliveText = "The ground beneath your feet rumbles." \ 
-            "You look up to a giant mechnical arm swing at you!" \
-            "You jump out of the way just in the nick of time." \
+            self.aliveText = "The ground beneath your feet rumbles."  
+            "You look up to a giant mechnical arm swing at you!" 
+            "You jump out of the way just in the nick of time." 
             "But the giant robot has not stopped its pursuit"
             self.deadText = "Sparks fly from the broken wires of the dead giant robot strewn across the ground in pieces."
         
         elif r < 0.80:
             self.enemies = enemies.StormTrooper() #place storm trooper 
-            self.aliveText = "You hear marching coming from the distance." \
+            self.aliveText = "You hear marching coming from the distance." 
             "You look up to see a familiar white suit and helmet...the Storm Troopers have arrived!"
-            self.deadText = "Broken laser guns, white suits, and helmets are sprawled across the ground."\
+            self.deadText = "Broken laser guns, white suits, and helmets are sprawled across the ground."
             "The Storm Troopers are dead, for now."
 
         elif r < 0.95:
             self.enemies = enemies.TRexDino() #place TRex
-            self.aliveText = "A loud scream rolls through the Field." \
-            "The stomping gets louder and faster followed by another scream..."\
+            self.aliveText = "A loud scream rolls through the Field." 
+            "The stomping gets louder and faster followed by another scream..."
             "You look up to see a TRex running towards you in the distance!"
             self.deadText = "The blood of the TRex rolls down the ground and the dead TRex's body rots beneath you."
         
         else:
             self.enemies = enemies.LaserDrone() #place Laser Drones
-            self.aliveText = "A hot laser burns the ground in front of your feet."\
+            self.aliveText = "A hot laser burns the ground in front of your feet."
             "Startled, you look up to see a Laser Drone coming right for you shooting at you!"
             self.deadText = "The punctured Laser Drone lies on the ground, broken and unresponsive."
         
         super().__init__(x,y)
 
-        def intro_text(self): # set the text to the right one according to if they are dead or alive
-            text = self.aliveText if self.enemies.isAlive() else self.deadText
-            return text
+    def introText(self): # set the text to the right one according to if they are dead or alive
+        text = self.aliveText if self.enemies.isAlive() else self.deadText
+        return text
         
-        def modifyPlayer(self, player): #update the player hp accordingly as player fights enemy
-            if self.enemies.isAlive():
-                player.hp = player.hp - self.enemies.damage
-                print("Enemy does {} damage. You have {} HP remaining.".format(self.enemies.damage, player.hp))
+    def modifyPlayer(self, player): #update the player hp accordingly as player fights enemy
+        if self.enemies.isAlive():
+            player.hp = player.hp - self.enemies.damage
+            print("Enemy does {} damage. You have {} HP remaining.".format(self.enemies.damage, player.hp))
 
 
 class VictoryTile(MapTile):
@@ -115,90 +120,6 @@ def tile_at(x, y):
     except IndexError:
         return None
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-world_dsl = """
-|EN|EN|VT|EN|EN|
-|EN|  |  |  |EN|
-|EN|FG|EN|  |TT|
-|TT|  |ST|FG|EN|
-|FG|  |EN|  |FG|
-"""
-
-
-def is_dsl_valid(dsl):
-    if dsl.count("|ST|") != 1:
-        return False
-    if dsl.count("|VT|") == 0:
-        return False
-    lines = dsl.splitlines()
-    lines = [l for l in lines if l]
-    pipe_counts = [line.count("|") for line in lines]
-    for count in pipe_counts:
-        if count != pipe_counts[0]:
-            return False
-
-    return True
-
-tile_type_dict = {"VT": VictoryTile,
-                  "EN": EnemyTile,
-                  "ST": StartTile,
-                  "FG": FindGoldTile,
-                  "TT": TraderTile,
-                  "  ": None}
-
-
-world_map = []
-
-start_tile_location = None
-
-
-def parse_world_dsl():
-    if not is_dsl_valid(world_dsl):
-        raise SyntaxError("DSL is invalid!")
-
-    dsl_lines = world_dsl.splitlines()
-    dsl_lines = [x for x in dsl_lines if x]
-
-    for y, dsl_row in enumerate(dsl_lines):
-        row = []
-        dsl_cells = dsl_row.split("|")
-        dsl_cells = [c for c in dsl_cells if c]
-        for x, dsl_cell in enumerate(dsl_cells):
-            tile_type = tile_type_dict[dsl_cell]
-            if tile_type == StartTile:
-                global start_tile_location
-                start_tile_location = x, y
-            row.append(tile_type(x, y) if tile_type else None)
-
-        world_map.append(row)
-
-
-def tile_at(x, y):
-    if x < 0 or y < 0:
-        return None
-    try:
-        return world_map[y][x]
-    except IndexError:
-        return None
 
 
 
